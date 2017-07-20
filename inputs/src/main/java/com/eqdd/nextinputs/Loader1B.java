3a2c5c1a31c5417c102a77fc1564772985e60401
lvzhihao <1030753080@qq.com>
2017-07-20 18:30:02 +0800

    init

--- inputs/src/main/java/com/eqdd/nextinputs/Loader1B.java
+++ inputs/src/main/java/com/eqdd/nextinputs/Loader1B.java
@@ -1,11 +1,13 @@
 package com.eqdd.nextinputs;
 
+import java.io.Serializable;
+
 /**
  * 数据懒加载器
  * @author 陈小锅 (yoojia.chen@gmail.com)
  * @since 1.8
  */
-public interface Loader1B<T> {
+public interface Loader1B<T> extends Serializable {
 
     /**
      * 在需要加载数据时,将在此方法中返回数据
--- inputs/src/main/java/com/eqdd/nextinputs/Loader2B.java
+++ inputs/src/main/java/com/eqdd/nextinputs/Loader2B.java
@@ -1,11 +1,13 @@
 package com.eqdd.nextinputs;
 
+import java.io.Serializable;
+
 /**
  * 数据懒加载器
  * @author 陈小锅 (yoojia.chen@gmail.com)
  * @since 1.8
  */
-public interface Loader2B<T>{
+public interface Loader2B<T> extends Serializable {
 
     T getFirstValue();
 
